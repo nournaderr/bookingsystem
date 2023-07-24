056aeae7bebece7b91162d069d79fdf276db1d86
unknown <adhamaldeib@gmail.com>
2023-07-24 11:38:08 +0300

    added some useState variables

--- src/Components/LoginCard.js
+++ src/Components/LoginCard.js
@@ -1,7 +1,10 @@
 import Lottie from "lottie-react";
 import ap from '../lotties/animation_ljzrexsp.json'
 import '../styles/Login.css'
+import { useState } from "react";
 export default function LoginCard (){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return(
         <div className="login-card">
             <div className="title">
@@ -16,9 +19,20 @@ export default function LoginCard (){
             <div className='wb'>Welcome Back<br/><span className='wb-t'>It's time to travel!</span></div>
             <form>
             <label>Email:</label>
-            <input name="email" placeholder='adham@gm.com'/>
+            <input 
+                name="email" 
+                placeholder='adham@gm.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
             <label>Password:</label>
-            <input name="password" type='password' placeholder='*****'/>
+            <input 
+                name="password" 
+                type='password' 
+                placeholder='*****'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
             <a className='forgot' href=''>forgot password?</a>
             <button className='login-btn'>Login</button>
             <a className='forgot center' href='/register'>CREATE ACCOUNT!</a>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,13 @@
 import '../styles/Nav.css'
+import {useNavigate} from 'react-router-dom'
 export default function Nav (){
+    const navigate = useNavigate();
+    const handleSignUp = () => {
+        navigate('/register')
+    }
+    const handleLogin = () => {
+        navigate('/login')
+    }
     return(
         <div className="nav">
             <h2>YOUR LOGO <br></br><span className='airline'>AIRLINE</span></h2>
@@ -8,8 +16,8 @@ export default function Nav (){
                 <li><a href="" className='nav-list'>Reservations</a></li>
                 <li><a href="" className='nav-list'>Hotels</a></li>
                 <li><a href="" className='nav-list'>Community</a></li>
-                <li><button className="sign-up-btn">Sign up</button></li>
-                <li><button className="login-btn-nav">Login</button></li>
+                <li><button className="sign-up-btn" onClick={handleSignUp}>Sign up</button></li>
+                <li><button className="login-btn-nav" onClick={handleLogin}>Login</button></li>
             </ul>
         </div>
     )
